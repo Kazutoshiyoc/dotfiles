caa98433a741632356ec50f35b9d23953025bd43
Kazutoshi Yoshii <kazutoshiyoc@gmail.com>
2022-06-28 18:06:32 +0900

    Neovim対応

--- setup.sh
+++ setup.sh
@@ -3,6 +3,10 @@
 # copy .vimrc file to home dir
 cp .vimrc ~/
 
+# copy Neovim configration file
+mkdir -p ~/.config/nvim
+cp .vimrc ~/.config/nvim/init.vim
+
 # git settings
 git config --global diff.tool vimdiff
 git config --global difftool.prompt false
